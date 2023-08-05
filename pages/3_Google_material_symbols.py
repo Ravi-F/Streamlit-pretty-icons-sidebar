8b37c7d470e1367b16d3aaef416a2bf47026475f
Socvest <82335390+Socvest@users.noreply.github.com>
2023-08-05 21:12:03 +0100

    Update 3_Google_material_symbols.py

--- pages/3_Google_material_symbols.py
+++ pages/3_Google_material_symbols.py
@@ -4,8 +4,13 @@ from custom_sidebar_icons import Set_Nav_Emojis as set_Nav
 
 st.write("Package has been loaded here")
 
-with open("./data.json", "r") as read_file:
-    emojis_list = json.load(read_file)
+emojis_list = [ 
+    {"emojiLibrary":"remix_icon","iconName":"ri-verified-badge-fill", "style":"", "elementID":"",}',
+    {"emojiLibrary":"icon_8", "iconName":"", "emojiObject":{'src':"https://img.icons8.com/fluency/48/smiling.png", 'height':50, 'width':50, 'alt':'smiling'}, "style":"", "elementID":"test-icon" },'
+    {"emojiLibrary":"tabler_icons", "iconName":"ti ti-adjustments-pin", "style":"", "elementID":"test-icon"},'
+    {"emojiLibrary":"Google_material_symbols", "iconName":"settings_accessibility", "style":"", "elementID":"test-icon"},'
+    {"emojiLibrary":"line_awesome", "iconName":"las la-dragon", "style":"", "elementID":"test-icon"},
+]
 
 emojis_render = set_Nav(emojis_list)
 emojis_render.show_me_the_icons_Render()
