68e09174ecc1f6b66897da6693f3996107e1365d
Socvest <82335390+Socvest@users.noreply.github.com>
2023-08-07 10:01:56 +0100

    Update remix_icon.py

--- remix_icon.py
+++ remix_icon.py
@@ -19,7 +19,7 @@ if "sideNav" not in st.session_state:
     st.session_state['sideNav'] = False
 
 
-query = "window.top.document.querySelectorAll('iframe[title='streamlitApp']')[0].contentDocument.head" 
+query = """window.top.document.querySelectorAll("iframe[title='streamlitApp']")[0].contentDocument.head""" 
 
 js = f"""
         <script>
