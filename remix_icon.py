ab98b804dbf27d2b364efbe269deb80d3472e5e7
Socvest <82335390+Socvest@users.noreply.github.com>
2023-08-07 10:03:37 +0100

    Update remix_icon.py

--- remix_icon.py
+++ remix_icon.py
@@ -24,10 +24,7 @@ query = """window.top.document.querySelectorAll("iframe[title='streamlitApp']")[
 js = f"""
         <script>
              toAppend = {query}
-            const GoogleEmoji = document.createElement("link");
-            GoogleEmoji.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0";
-            GoogleEmoji.rel = "stylesheet";
-            toAppend.appendChild(GoogleEmoji)
+           
         
             console.log("{query}", toAppend)
         </script>
