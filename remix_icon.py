04b44135dbd2e02ffa906f99c638b77818b4d670
Socvest <82335390+Socvest@users.noreply.github.com>
2023-08-07 09:40:56 +0100

    Update remix_icon.py

--- remix_icon.py
+++ remix_icon.py
@@ -19,7 +19,7 @@ if "sideNav" not in st.session_state:
     st.session_state['sideNav'] = False
 
 
-query = "iframe[class*='styles_iframe'] html head"
+query = "iframe[class*='styles_iframe'] #document html head"
 
 js = f"""
         <script>
