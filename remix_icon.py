b63866180e68ac164f86307bce8767cf492f6660
Socvest <82335390+Socvest@users.noreply.github.com>
2023-08-07 09:25:19 +0100

    Update remix_icon.py

--- remix_icon.py
+++ remix_icon.py
@@ -23,7 +23,7 @@ query = "iframe[title='streamlitApp']"
 
 js = f"""
         <script>
-            toAppend = parent.document.querySelectorAll('{query}')
+            toAppend = parent.document.querySelectorAll({query})
             const GoogleEmoji = document.createElement("link");
             GoogleEmoji.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0";
             GoogleEmoji.rel = "stylesheet";
