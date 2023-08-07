a0a35b43971f6e2daf466d1e9892720ea14c3ce7
Socvest <82335390+Socvest@users.noreply.github.com>
2023-08-07 09:25:46 +0100

    Update remix_icon.py

--- remix_icon.py
+++ remix_icon.py
@@ -28,7 +28,6 @@ js = f"""
             GoogleEmoji.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0";
             GoogleEmoji.rel = "stylesheet";
             console.log(toAppend)
-            
         </script>
     """
 placeholder = st.empty()
