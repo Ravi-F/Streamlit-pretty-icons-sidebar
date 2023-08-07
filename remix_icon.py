63a4ae7f8ed6cf99e2d902a9f212636ffc879719
Socvest <82335390+Socvest@users.noreply.github.com>
2023-08-07 09:33:16 +0100

    Update remix_icon.py

--- remix_icon.py
+++ remix_icon.py
@@ -19,7 +19,7 @@ if "sideNav" not in st.session_state:
     st.session_state['sideNav'] = False
 
 
-query = "iframe[title='streamlitApp']"
+query = "iframe[class*='styles_iframe']"
 
 js = f"""
         <script>
